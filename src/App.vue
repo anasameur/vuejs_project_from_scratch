316381ef035223d8ec507656055aa3d525d06fc4
Anas AMEUR <anas.ameur@engie.com>
2018-11-21 10:47:16 +0100

    nouvelle config webpack

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
-        <h1>Hello World!</h1>
+    <div class="full-width center-content">
+        <hello-component name="World" />
     </div>
 </template>
+
+<script>
+import HelloComponent from './components/HelloComponent.vue';
+export default {
+  components: {
+    HelloComponent,
+  },
+};
+</script>
+<style scoped>
+.full-width {
+  width: 100%;
+}
+.center-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
